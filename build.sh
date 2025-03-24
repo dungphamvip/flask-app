682da6ae8d87c89d16e01edc94fcc7f44385f5cd
dungphamvip <dungphamvip@gmail.com>
2025-03-24 18:40:36 +0700

    Fix Render deployment configuration

--- build.sh
+++ build.sh
@@ -2,18 +2,14 @@
 # exit on error
 set -o errexit
 
-# Install Python if it doesn't exist
-if ! command -v python3.9 &> /dev/null
-then
-    apt-get update
-    apt-get install -y python3.9
-fi
-
 # Upgrade pip
-python3.9 -m pip install --upgrade pip
+python -m pip install --upgrade pip
 
 # Install dependencies
 pip install -r requirements.txt
 
-# Make sure gunicorn is installed in the correct path
-pip install gunicorn --user
+# Install gunicorn
+pip install gunicorn
+
+# Make sure we're in the right directory
+cd /opt/render/project/src
